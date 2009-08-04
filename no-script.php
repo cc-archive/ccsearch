ad7101843b74202f2cdfa82323c845aafc636174
pyrak <pyrak@aec4f576-4910-0410-beeb-ecb0427469e7>
2009-08-04 00:11:06 +0000

    fixed noscript behavior bug with ffx, and made the noscript message prettier.
    git-svn-id: http://code.creativecommons.org/svnroot/ccsearch/trunk@13227 aec4f576-4910-0410-beeb-ecb0427469e7

--- no-script.php
+++ no-script.php
@@ -1,1 +1,24 @@
-<noscript><p>JAVASCRIPT IS REQUIRED FOR USING CCSEARCH</p></noscript>
+<html>
+<head>
+<title></title>
+<style type="text/css">
+html, body{
+   width: 100%;
+   text-align: center;
+   font-family:arial,sans-serif;
+}
+
+#main{
+   display: inline-block;
+   background-color: yellow;
+   text-align: center;
+   padding: 10px;
+   border: 1px dashed gray;
+   font-size: 1.2em;
+}
+</style>
+</head>
+<body>
+<noscript><p id="main">ERROR: ccSearch requires Javascript.</p></noscript>
+</body>
+</html>
