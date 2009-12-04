61d796ea2f4bad4196338527db912ce4e8cc8398
bse <bse@aec4f576-4910-0410-beeb-ecb0427469e7>
2009-12-04 01:30:25 +0000

    Merged revisions 13748 via svnmerge from svn+ssh://svn@code.creativecommons.org/svnroot/ccsearch/trunk
    ........
      r13748 | bse | 2009-12-04 01:28:02 +0000 (Fri, 04 Dec 2009) | 1 line
      
      Overhauled intro text - haven't touched i18n, scared.
    ........
    
    
    git-svn-id: http://code.creativecommons.org/svnroot/ccsearch/branches/production@13749 aec4f576-4910-0410-beeb-ecb0427469e7

--- intro.php
+++ intro.php
@@ -80,7 +80,7 @@ font-stretch:normal;
 font-style:normal;
 font-variant:normal;
 font-weight:normal;
-line-height:1.5em;
+line-height:1.5;
 font-size: 1.5em;
 padding: 10px 100px 20px 100px;
 	}
@@ -95,7 +95,9 @@ font-size: 1.1em;
 font-style: normal;
 }
 #thefineprint{
-	margin-bottom: 30px;
+	margin-top: 3em;
+	font-size: 90%;
+	line-height: 1.8;
 }
 .aboutcc{
 	margin-left: 45%;
@@ -114,10 +116,19 @@ border-radius: 5px;
 padding: 5px 10px 5px 10px;
 }
 
-
-h2{
- color: #808080;
+a { 
+	color: #333; 
+	text-decoration: none;
 }
+
+a:hover { text-decoration: underline; }
+
+h2 {
+ color: #808080;
+	font-family: "Helvetica", Arial, sans-serif;
+}
+
+
 	</style>
 	
 </head>
@@ -127,24 +138,13 @@ h2{
 <h1>Search for Creative Commons Licensed Material</h1>
 -->
 
-<h2><?php echo _('Enter a Search Query Above To Get Started') ?></h2>
+<h2 class="learn"><?php echo _('Creative Commons is a <a href="https://support.creativecommons.org/donate?utm_source=search&utm_medium=intro&utm_campaign=fall2009" target="_parent">nonprofit</a> organization <br/><a href="http://creativecommons.org/about/what-is-cc" target="_parent">Learn more</a> &mdash; <a href="https://support.creativecommons.org/donate?utm_source=search&utm_medium=intro&utm_campaign=fall2009" target="_parent">Support our work</a>') ?></h2>
 
 <p id="thefineprint">
 <?php echo _('Please note that search.creativecommons.org is <em>not a search engine</em>, but rather offers convenient access to search services provided by other independent organizations. CC has no control over the results that are returned. <em>Do not assume that the results displayed in this search portal are under a CC license</em>. You should always verify that the work is actually under a CC license by following the link. Since there is no registration to use a CC license, CC has no way to determine what has and hasn\'t been placed under the terms of a CC license. If you are in doubt you should contact the copyright holder directly, or try to contact the site where you found the content.') ?>
 </p>
 
-<h2><?php echo _('About Creative Commons') ?></h2>
 
-<embed src="http://blip.tv/play/gpxSkdkzg9ky" type="application/x-shockwave-flash" height="270" allowscriptaccess="always" allowfullscreen="true" id="aboutcc_vid"></embed> 
-<p class="aboutcc">
-<?php echo _('Creative Commons is a <em>nonprofit</em> corporation dedicated to making it easier for people to share and build upon the work of others, consistent with the rules of copyright.') ?>
-</p>
-<p class="aboutcc">
-<?php echo _('We provide <em>free</em> licenses and other legal tools to mark creative work with the freedom the creator wants it to carry, so others can share, remix, use commercially, or any combination thereof.') ?>
-</p>
-<p class="aboutcc">
-<?php echo _('We rely on the generous support of forward-thinkers like you to help us continue the work we do. Consider <a href="https://support.creativecommons.org/donate?utm_source=search&utm_medium=intro&utm_campaign=fall2009" target="_parent">donating to our annual campaign</a>, today!'); ?>
-</p>
 
 </body>
 </html>
